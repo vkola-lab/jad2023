b8b31ed87a0b558711b0c36dcb51fd8c24e534e4
Cody Karjadi <ckarjadi@bu.edu>
2021-11-02 10:43:25 -0400

    train_ve.py: added pad_mni_brain(). fixed input of data to the ImageEncoder() by padding properly.

--- train_ve.py
+++ train_ve.py
@@ -16,6 +16,17 @@ from net_id import ImageDecoder
 from net_ve import VoiceEncoder
 from read_txt import select_task, get_pt_file
 
+def pad_mni_brain(mni_brain):
+	"""
+	padding mni brain to proper dimensions;
+	"""
+	torch_array = torch.from_numpy(nib.load(mni_brain).get_fdata())
+	height, width, depth = torch_array.shape
+	torch_array = torch.unsqueeze(torch_array, 0)
+	torch_array = torch.unsqueeze(torch_array, 0)
+	torch_array = torch_array[:,:,0:(height - 1),0:(width-2),0:(depth-1)]
+	return nn.functional.pad(torch_array, (4, 4, 0, 0, 4, 4), "constant", 0)
+
 def gen_mri_latent_vectors(csv_in, image_encoder):
 	"""
 	generate the mri latent vectors
@@ -25,7 +36,7 @@ def gen_mri_latent_vectors(csv_in, image_encoder):
 	for row in yield_csv_data(csv_in):
 		mni_brains.add(row['mni_brain'])
 	for mni_brain in mni_brains:
-		torch_array = torch.from_numpy(nib.load(mni_brain).get_fdata())
+		torch_array = pad_mni_brain(mni_brain).to(torch.float)
 		mni_brain_to_vector[mni_brain] = image_encoder(torch_array)
 	return mni_brain_to_vector
 
@@ -55,8 +66,8 @@ def main():
 	image_encoder = ImageEncoder()
 	image_decoder = ImageDecoder()
 	image_autoencoder = nn.Sequential(image_encoder, image_decoder)
-	image_autoencoder.load_state_dict(torch.load(img_pt))
-
+	img_dict = torch.load(img_pt)['state_dict']
+	image_autoencoder.load_state_dict(img_dict)
 	loaded_image_encoder = image_autoencoder[0]
 	mni_brain_to_vector = gen_mri_latent_vectors(csv_info, loaded_image_encoder)
 	for mni_brain, mni_vector in mni_brain_to_vector.items():
