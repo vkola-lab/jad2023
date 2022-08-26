5628c4c2a5fd45729301f9778969dc0c570da4fc
Cody Karjadi <ckarjadi@bu.edu>
2022-08-26 10:17:34 -0400

    binary_model.py, binary_train.py - adjusting model and train files to be specific to binary classification paradigms. misc.py: added binary classification helper functions

---
+++ binary_model.py
@@ -0,0 +1,164 @@
+"""
+model.py
+overarching model class that can take in any neural network object;
+"""
+import sys
+from collections import defaultdict
+import torch
+import numpy as np
+from tqdm import tqdm
+from audio_dataset import collate_fn
+from net_ve import VoiceEncoder
+from misc import calc_performance_metrics, show_performance_metrics
+
+class BinaryModel:
+	"""
+	overarching model class;
+	"""
+
+	def __init__(self, n_concat, nn=None, device='cpu'):
+		"""
+		init;
+		"""
+		self.n_concat = n_concat
+		self.nn = nn if nn is not None else VoiceEncoder()
+		self.set_nn_device(device)
+		self.device = device
+
+	def set_nn_device(self, device):
+		"""
+		call model.to() and set device;
+		check range of gpu devices (currently have 4 gpus);
+		"""
+		assert device in ['cpu', 0, 1, 2, 3], 'Invalid device.'
+		self.nn.to(device)
+		self.nn.device = device
+
+	def fit(self, dset_trn, dset_vld, dir_rsl, **kwargs):
+		"""
+		fit method
+		"""
+		n_epoch = kwargs.get('n_epoch', 32)
+		b_size = kwargs.get('b_size', 4)
+		learning_rate = kwargs.get('learning_rate', 0.001)
+		weights = kwargs.get('weights', [])
+		debug_stop = kwargs.get('debug_stop', False)
+		loss_fn = kwargs.get('loss_fn', torch.nn.CrossEntropyLoss)
+		dataloader_kw = {'batch_size': b_size, 'shuffle': False, 'num_workers': 1,
+			'collate_fn': collate_fn, 'sampler': None}
+		dldr_trn = torch.utils.data.DataLoader(dset_trn, **dataloader_kw)
+		weights = torch.FloatTensor(weights).cuda(self.device)
+		opt = torch.optim.Adam(self.nn.parameters(), lr=learning_rate)
+		loss_fn = loss_fn(weight=weights)
+		train_epochs_kw = {'debug_stop': debug_stop, 'n_epoch': n_epoch, 'opt': opt,
+			'loss_fn': loss_fn, 'dir_rsl': dir_rsl}
+		self.train_epochs(dset_trn, dset_vld, dldr_trn, **train_epochs_kw)
+
+	def train_epochs(self, dset_trn, dset_vld, dldr_trn, **kwargs):
+		"""
+		go thru epochs and train;
+		"""
+		debug_stop = kwargs.get('debug_stop')
+		n_epoch = kwargs.get('n_epoch')
+		opt = kwargs.get('opt')
+		loss_fn = kwargs.get('loss_fn')
+		dir_rsl = kwargs.get('dir_rsl')
+		if not debug_stop:
+			for epoch in range(n_epoch):
+				## set model to training mode
+				self.nn.train()
+				cum_loss, count = 0, 0
+				## training loop
+				with tqdm(total=len(dset_trn), desc=f'Epoch {epoch} (TRN)',
+					ascii=True, bar_format='{l_bar}{r_bar}', file=sys.stdout) as pbar:
+					for Xs, ys, *_ in dldr_trn:
+						self.nn.reformat(Xs, self.n_concat)
+						ys = torch.tensor(ys, dtype=torch.float32, device=self.nn.device)
+						self.nn.zero_grad()
+						scores, loss = self.nn.get_scores_loss(Xs, ys, loss_fn)
+						loss.backward()
+						opt.step()
+						pred = torch.argmax(scores, 1)
+						## accumulated loss
+						cum_loss += loss.data.cpu().numpy() * len(ys)
+						## accumulated no. of correct predictions
+						cum_corr += (pred == ys).sum().data.cpu().numpy()
+						## accumulated no. of processed samples
+						count += len(ys)
+						## update statistics and progress bar
+						pbar.set_postfix({
+							'loss': f'{(cum_loss / count):.6f}',
+							'acc' : f'{(cum_corr / count):.6f}',
+						})
+						pbar.update(len(ys))
+				## forward validation dataset
+				scr = self.prob(dset_vld)
+				met = calc_performance_metrics(scr, dset_vld.labels)
+				print('Audio-level validation performance:')
+				show_performance_metrics(met)
+				print()
+				# save model
+				if np.isnan(met['mcc']):
+					continue
+				if vld_mcc <= met['mcc']:
+					vld_mcc = met['mcc']
+					self.save_model(f'{dir_rsl}/tmp.pt')
+			# load best model
+			if dset_vld is not None and vld_mcc != -1:
+				self.load_model(f'{dir_rsl}/tmp.pt')
+
+	def prob(self, dset, b_size=16, eval_collate_fn=collate_fn):
+		"""
+		calc model output
+		"""
+		_, rsl = self.eval(dset, b_size=b_size, eval_collate_fn=eval_collate_fn)
+		return np.exp(rsl)[:,1] / np.sum(np.exp(rsl), axis=1)
+
+	def eval(self, dset, **kwargs):
+		"""
+		evaluate model;
+		"""
+		b_size = kwargs.get('b_size', 16)
+		debug_stop = kwargs.get('debug_stop', False)
+		eval_collate_fn = kwargs.get('eval_collate_fn', collate_fn)
+		if debug_stop:
+			return []
+		self.nn.eval()
+		dl_dr_kwargs = {'batch_size': b_size,
+				  'shuffle': False,
+				  'num_workers': 1,
+				  'collate_fn': eval_collate_fn}
+		dldr = torch.utils.data.DataLoader(dset, **dl_dr_kwargs)
+		# list to store result (i.e. all outputs)
+		x_fp_to_rsl = defaultdict(dict)
+		all_results = []
+		# evaluation loop
+		with torch.set_grad_enabled(False):
+			with tqdm(total=len(dset), desc='Epoch ___ (EVL)', ascii=True,
+				bar_format='{l_bar}{r_bar}', file=sys.stdout) as pbar:
+				for Xs, _, x_filepaths, start_end_list in dldr:
+					self.nn.reformat(Xs, self.n_concat)
+					out = self.nn.get_scores(Xs)
+					# append batch outputs to result
+					results = out.data.cpu().numpy()
+					all_results.append(results)
+					for idx, x_fp in enumerate(x_filepaths):
+						start_end = start_end_list[idx]
+						assert start_end not in x_fp_to_rsl[x_fp], f'{x_fp}, {start_end}'
+						x_fp_to_rsl[x_fp][start_end] = results[idx]
+					# progress bar
+					pbar.update(len(Xs))
+		# concatenate all batch outputs
+		return x_fp_to_rsl, results
+
+	def save_model(self, filepath):
+		"""
+		save model as a file;
+		"""
+		torch.save(self.nn, filepath)
+
+	def load_model(self, filepath):
+		"""
+		load a model file;
+		"""
+		self.nn = torch.load(filepath)
---
+++ binary_train.py
@@ -0,0 +1,78 @@
+"""
+Created on Wed Oct 27 11:09:36 2021
+
+@author: cxue2
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+import sys
+import torch
+import train_func as tf
+from handle_input import get_args
+from misc import gen_seed_dirs
+from read_txt import select_task
+
+def main():
+	"""
+	main entrypoint;
+	"""
+	args = get_args(sys.argv[1:])
+	args = {k: v for k, v in args.items() if v is not None}
+	task_csv_txt = args.get('task_csv_txt', 'task_csvs.txt')
+	task_id = args.get('task_id', 0)
+	device = int(args.get('device', 0))
+	n_epoch = int(args.get('n_epoch', 1))
+	num_seeds = int(args.get('num_seeds', 1))
+	num_folds = int(args.get('num_folds', 5))
+	do_rand_seg = args.get('do_rand_seg')
+	num_pt_segments = int(args.get('num_pt_segments', 10))
+	pt_segment_root = args.get('pt_segment_root')
+	seg_min = int(args.get('seg_min', 5))
+	learning_rate = float(args.get('learning_rate', 1e-3))
+	negative_loss_weight = float(args.get('negative_loss_weight', 1))
+	positive_loss_weight = float(args.get('positive_loss_weight', 1))
+	weights = [negative_loss_weight, positive_loss_weight]
+	debug_stop = args.get('debug_stop')
+	no_save_model = args.get('no_save_model')
+	no_write_fold_txt = args.get('no_write_fold_txt')
+
+	final_args = {'task_id': task_id,
+		'device': device, 'n_epoch': n_epoch,
+		'num_seeds': num_seeds, 'num_folds': num_folds,
+		'do_rand_seg': do_rand_seg, 'num_pt_segments': num_pt_segments,
+		'pt_segment_root': pt_segment_root, 'seg_min': seg_min,
+		'learning_rate': learning_rate, 'weights': weights,
+		'no_save_model': no_save_model}
+
+	csv_info, ext = select_task(task_id, task_csv_txt)
+
+	seed_to_dir = gen_seed_dirs(num_seeds, ext, n_epoch)
+
+	for seed, dir_rsl in seed_to_dir.items():
+		trn_dir, vld_dir = tf.gen_dirs(dir_rsl)
+		for vld_idx in range(num_folds):
+			for tst_idx in range(num_folds):
+				if vld_idx == tst_idx:
+					continue ## vld and tst fold can't be the same?
+				dset_kw = tf.set_dset_kw(num_folds, vld_idx, tst_idx, seed, do_rand_seg,
+					num_pt_segments, pt_segment_root, seg_min)
+
+				dset_trn, dset_vld, dset_tst = tf.gen_audio_datasets(csv_info, dset_kw)
+				ys_len = 2 ## ?
+				loss_fn = torch.nn.CrossEntropyLoss
+				model_obj = tf.fit_model(device, n_epoch, learning_rate, weights, debug_stop,
+					dset_trn, dset_vld, dir_rsl, loss_fn, ys_len)
+
+				vld_tst = f'vld_{vld_idx}_tst_{tst_idx}'
+
+				tf.save_model(no_save_model, dir_rsl, model_obj, vld_tst)
+
+				results = model_obj.prob(dset_tst, b_size=16)
+				df_dat = dset_tst.df_dat
+				df_dat['score'] = results
+				tf.save_csvs(df_dat, dset_trn, dset_vld, dir_rsl, seed, vld_tst, trn_dir, vld_dir)
+				tf.write_fold_txt(no_write_fold_txt, dir_rsl, vld_tst, ext, seed, vld_idx, tst_idx,
+					final_args, dset_trn, dset_vld, dset_tst)
+
+if __name__ == '__main__':
+	main()
